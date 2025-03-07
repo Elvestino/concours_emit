14964549ce04aeb2632ac9e29f536aff83be86b5
Dally Maminomenjanahary <130447794+Dally07@users.noreply.github.com>
2025-03-07 23:45:20 +0300

    ajout du middleware sur chaque action dans la partie admin

--- Backend/routes/participantRoutes.js
+++ Backend/routes/participantRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const participantController = require('../controllers/participantController');
+const authMiddleware = require("../middleware/authMiddleware");
 
 
 
 router.post('/upload', participantController.upload, participantController.uploadFiles);
 
-router.get('/admin/listes', participantController.getParticipants );
+router.get('/admin/listes',authMiddleware, participantController.getParticipants );
 
-router.delete('/participant/:id', participantController.deleteParticipant );
+router.delete('/participant/:id', authMiddleware, participantController.deleteParticipant , );
 
-router.patch('/participant/:id/accept', participantController.acceptedParticipant );
+router.patch('/participant/:id/accept', authMiddleware, participantController.acceptedParticipant,  );
 
-router.put('/participant/actualise', participantController.resetParticipantAccepted );
+router.put('/participant/actualise', authMiddleware, participantController.resetParticipantAccepted,  );
 
 module.exports = router;
